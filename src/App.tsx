df0768745d965ed8272bc84a915283ac2d6dd992
Joshua Covington <covingtonjoshua7@gmail.com>
2024-09-30 08:59:05 -0400

    added color constant
    added color constant to show on colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+const colors = [
+    {
+        hex: 'EFDECD',
+        name: 'Almond',
+        comp: [
+            { hex: 'ADC6E0', name: 'Periwinkle Gray' },
+            { hex: 'D8E6F5', name: 'Link Water' },
+            { hex: 'FFFFFF', name: 'White' },
+        ],
+    },
+    {
+        hex: 'CD9575',
+        name: 'Antique Brass',
+        comp: [
+            { hex: '649DBE', name: 'Cerulean Frost' },
+            { hex: '88BCDA', name: 'Half Baked' },
+            { hex: 'EFC9B3', name: 'Desert Sand' },
+        ],
+    },
+];
+
 const App = () => {
     return <h1>Hello World</h1>;
 };
